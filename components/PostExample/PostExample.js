7ee6ff1b7658e7c34af237fb78892c350ffb3237
hayhaydz <haydon1900@gmail.com>
2022-01-18 16:21:41 +0000

    Completed: Example post block. Todo: Unique interaction options and Feedback exploration

--- components/PostExample/PostExample.js
+++ components/PostExample/PostExample.js
@@ -1,15 +1,35 @@
+import { Heart, MessageSquare, Share } from 'react-feather';
+import { Button } from '../';
 
 const PostExample = () => {
     return (
-        <article className="bg-secondary-800 w-full h-[440px] rounded-lg">
-            <div className="flex">
-                <span className=""></span>
-                <div>
-                    <span></span>
-                    <span></span>
+        <div>
+            <article className="bg-secondary-800 w-full h-[490px] rounded-lg p-6 flex flex-col">
+                <div className="flex mb-4">
+                    <span className="w-[48px] h-[48px] bg-secondary-600 rounded-full aspect-square mr-6"></span>
+                    <div>
+                        <span className="block w-64 h-5 bg-secondary-600 rounded-full mb-2"></span>
+                        <span className="block w-32 h-4 bg-secondary-700 rounded-full"></span>
+                    </div>
                 </div>
+                <div className="mb-4">
+                    {
+                        [...Array(4)].map((elementInArray, index) => ( 
+                            <span className="block w-full h-5 bg-secondary-600 rounded-full mb-2" key={index}></span>
+                        ))
+                    }
+                    <span className="block w-40 h-5 bg-secondary-600 rounded-full"></span>
+                </div>
+                <div className="w-full h-full">
+                    <span className="block w-full h-full bg-secondary-700 rounded-lg"></span>
+                </div>
+            </article>
+            <div className="flex gap-2 mt-4">
+                <Button ><Heart size={18}/> <span className="font-medium ml-2">Like</span></Button>
+                <Button ><MessageSquare size={18}/> <span className="font-medium ml-2">Comment</span></Button>
+                <Button ><Share size={18}/> <span className="font-medium ml-2">Share</span></Button>
             </div>
-        </article>
+        </div>
     )
 }
 export default PostExample
--- components/index.js
+++ components/index.js
@@ -3,3 +3,4 @@ export { default as Header } from './Header/Header';
 export { default as InteractionGrid } from './InteractionGrid/InteractionGrid';
 export { default as PostExample } from './PostExample/PostExample';
 export { default as ResponseGrid } from './ResponseGrid/ResponseGrid';
+export { default as Button } from './Button/Button';
