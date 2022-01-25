b578b66ccd33813c37a130c017eac520caaaf143
hayhaydz <haydon1900@gmail.com>
2022-01-25 16:53:51 +0000

    Added a favicon

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -6,7 +6,13 @@ const Layout = ({ children }) => {
             <Head>
                 <title>Understanding Social Media Interaction</title>
                 <meta name="description" content="How can we better improve the experience of giving and receiving on social media?" />
-                <link rel="icon" href="/favicon.ico" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#4900d8" />
+                <meta name="msapplication-TileColor" content="#130d23" />
+                <meta name="theme-color" content="#ffffff" />
                 <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&family=Syncopate:wght@400;700&display=swap" rel="stylesheet" />
                 <meta name="viewport" content="width=device-width" />
             </Head>
